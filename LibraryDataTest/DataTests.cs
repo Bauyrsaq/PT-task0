eb2f28c5d2e3ccae07ae3bb15bad4d1fbfa5c654
Bauyrsaq <talapali20@gmail.com>
2023-06-05 11:16:53 +0200

    database testing

--- LibraryDataTest/DataTests.cs
+++ LibraryDataTest/DataTests.cs
@@ -21,10 +21,10 @@ namespace LibraryDataTest
         public static void ClassInitializeMethod(TestContext context)
         {
             string _DBRelativePath = @"LibraryDBTest.mdf";
-            string _projectRootDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            string _projectRootDir = Directory.GetCurrentDirectory();
             string _DBPath = Path.Combine(_projectRootDir, _DBRelativePath);
             FileInfo _databaseFile = new FileInfo(_DBPath);
-            Assert.IsTrue(_databaseFile.Exists, $"{Environment.CurrentDirectory}");
+            Assert.IsTrue(_databaseFile.Exists, $"Database file does not exist at: {_projectRootDir}");
             connectionString = $@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={_DBPath};Integrated Security = True; Connect Timeout = 30;";
         }
 
@@ -33,7 +33,14 @@ namespace LibraryDataTest
         {
             int userId = 1;
 
-            
+            _dataRepository.AddUser(userId, "John", "Wick");
+
+            IUser user = _dataRepository.GetUser(userId);
+
+            Assert.IsNotNull(user);
+            Assert.AreEqual(userId, user.Id);
+            Assert.AreEqual("John", user.Name);
+            Assert.AreEqual("Wick", user.Surname);
 
         }
     }
