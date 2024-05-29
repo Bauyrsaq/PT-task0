2fbecdc3f5f22151f29a5020b23bdc53e01d91f3
Bauyrsaq <talapali20@gmail.com>
2024-05-29 15:43:42 +0200

    add user class

---
+++ LibraryData/DataAPI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryData
+{
+    public partial class DataAPI
+    {
+        public List<User> Users = new List<User> ();
+    }
+}
---
+++ LibraryData/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryData
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        
+        public User(int id, string name, string surname)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+        }
+
+        public override string ToString()
+        {
+            return Id + ": " + Name + " " + Surname;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashId = Id.GetHashCode();
+            int hashName = Name.GetHashCode();
+            int hashSurname = Surname.GetHashCode();
+            return hashId ^ hashName ^ hashSurname;
+        }
+    }
+}
